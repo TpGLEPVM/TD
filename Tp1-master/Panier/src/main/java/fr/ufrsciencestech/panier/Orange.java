ffa9259f783637e49f98146e4efad86a1d2ca535
bt479917 <bt479917@MI103-04>
2018-10-11 11:03:28 +0200

    Initial commit

--- Tp1-master/Panier/src/main/java/fr/ufrsciencestech/panier/Orange.java
+++ Tp1-master/Panier/src/main/java/fr/ufrsciencestech/panier/Orange.java
@@ -9,7 +9,7 @@ import fr.ufrsciencestech.panier.OrangeNegException;
  *
  * @author vb394093
  */
-public class Orange implements Fruits {
+public class Orange extends FruitSimple {
     private double prix;
     private String origine;
     
